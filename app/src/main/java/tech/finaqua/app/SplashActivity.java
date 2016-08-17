31b96be368db3bfdfb65185eec7bd7772a253f9a
Abdullah AlShaikh <Abdullah@AlShai5.com>
2016-08-17 16:26:41 +0300

    Splash Activity Added

---
+++ app/src/main/java/tech/finaqua/app/SplashActivity.java
@@ -0,0 +1,41 @@
+package tech.finaqua.app;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/**
+ * An example full-screen activity that shows and hides the system UI (i.e.
+ * status bar and navigation/system bar) with user interaction.
+ */
+public class SplashActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        getSupportActionBar().hide();
+
+        Thread timerThread = new Thread(){
+            public void run(){
+                try{
+                    sleep(3000);
+                }catch(InterruptedException e){
+                    e.printStackTrace();
+                }finally{
+                    //Intent intent = new Intent(SplashScreen.this,MainActivity.class);
+                    //startActivity(intent);
+                    startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                }
+            }
+        };
+        timerThread.start();
+
+    }
+
+    @Override
+    protected void onPause() {
+        // TODO Auto-generated method stub
+        super.onPause();
+        finish();
+    }
+}
